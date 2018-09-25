399636938a679e03331609fe7c1c0df3e97da323
melissachamilton <melissac.hamilton1@gmail.com>
2018-09-25 10:46:19 -0400

    trying heroku fix

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { User } = require('../db/schema')
+const { User } = require('../db/schema.js')
 var method = require('method-override')
 
 /* GET users listing. */
